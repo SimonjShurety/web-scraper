91372de664d0b0a4c307e35c16a694d61b9faa41
SimonjShurety <simonjshurety@gmail.com>
2022-10-07 22:30:37 -0300

    Change URL and scraper class and elements

--- index.js
+++ index.js
@@ -5,19 +5,19 @@ const express = require("express");
 
 const app = express();
 
-const url = "https://www.theguardian.com/uk";
+const url = "https://www.microsoft.com/pt-br";
 
 axios(url).then(response => {
   const html = response.data
   const $ = cheerio.load(html)
   const articles = []
 
-  $('.fc-item__title', html).each(function () {
+  $('.text-break-keep-all', html).each(function () {
     const title = $(this).text()
-    const url = $(this).find('a').attr('href')
+    // const url = $(this).find('a').attr('href')
     articles.push({
-      title,
-      url
+      title
+      // url
     })
   })
   console.log(articles)
